66c2df85c195a11d4022549c9857d22e4f328be8
tonantr <tonantruong@hotmail.com>
2023-04-05 21:28:03 -0400

    Query Selector Methods

---
+++ index.js
@@ -0,0 +1,9 @@
+const li2 = document.querySelector(".ranked-list li ul li");
+li2;
+
+
+const div4 = document.querySelector(".unranked-list li div");
+div4;
+
+const element = document.getElementById("app").querySelectorAll("ul.ranked-list li");
+element;
